f4566dbeba137c33f7871046b4ff32d72b6d41f1
kusa <foxys130824@gmail.com>
2026-01-20 23:03:02 +0300

    added basic anime info

--- src/components/AnimeInfo.jsx
+++ src/components/AnimeInfo.jsx
@@ -6,7 +6,20 @@ function AnimeInfo({ data }) {
       {data && data.Page && (
         <div>
           {data.Page.media.map((anime) => (
-            <h3>{anime.description}</h3>
+            <div>
+              <h2>{anime.title.english}</h2>
+              <img src={anime.coverImage.large} alt="" />
+              <section className="description">
+                <h3>Description</h3>
+                <p>{anime.description}</p>
+              </section>
+              <section className="info">
+                <p>Episodes: {anime.episodes}</p>
+                <p>Score: {anime.averageScore}</p>
+                <p>Genres: {anime.genres.join(", ")}</p>
+                <p>End year: {anime.endDate.year}</p>
+              </section>
+            </div>
           ))}
         </div>
       )}
