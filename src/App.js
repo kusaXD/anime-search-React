586245088d587e4f87f00a1df2e0b2fe38611ccd
kusa <foxys130824@gmail.com>
2026-01-24 15:11:58 +0300

    added button search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "@apollo/client/react";
+import { useLazyQuery } from "@apollo/client/react";
 import { ANIME } from "./Appolo/anime";
 import SearchForm from "./components/SearchForm";
 import AnimeList from "./components/AnimeList";
@@ -7,19 +7,12 @@ import "./styles/App.css";
 
 function App() {
   const [value, setValue] = useState("");
-  const { loading, error, data } = useQuery(ANIME, {
-    variables: {
-      mode: "no-cors",
-      search: value,
-      page: 1,
-      perPage: 1,
-    },
-  });
+  const [getAnime, { loading, error, data }] = useLazyQuery(ANIME);
 
   return (
     <div className="App">
       <h1>Anime Search</h1>
-      <SearchForm value={value} setValue={setValue} />
+      <SearchForm value={value} setValue={setValue} getAnime={getAnime} />
 
       {loading && <p>loading...</p>}
       {error && <p>error: {error.message}</p>}
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import MyInput from "./UI/Input/MyInput";
 import MyButton from "./UI/Button/MyButton";
 
-function SearchForm(props) {
+function SearchForm({ value, setValue, getAnime }) {
+  function getData(e) {
+    e.preventDefault();
+    getAnime({
+      variables: {
+        mode: "no-cors",
+        search: value,
+        page: 1,
+        perPage: 1,
+      },
+    });
+  }
+
   return (
     <form className="Myform">
-      <MyInput
-        value={props.value}
-        onChange={(e) => props.setValue(e.target.value)}
-      />
-      <MyButton>search</MyButton>
+      <MyInput value={value} onChange={(e) => setValue(e.target.value)} />
+      <MyButton onClick={(e) => getData(e)}>search</MyButton>
     </form>
   );
 }
